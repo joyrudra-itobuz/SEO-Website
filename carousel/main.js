27388f0c7424d157b3c45f9b5bc57ebc6d5e1edc
joyrudra-itobuz <joyrudra@itobuz.com>
2023-02-17 16:31:16 +0530

    Responsive Changes

--- carousel/main.js
+++ carousel/main.js
@@ -1,6 +1,14 @@
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
-  spaceBetween: 30,
+  spaceBetween: 50,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
