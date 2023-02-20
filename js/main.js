61c5d84f56bf6e1342ac87c073f32c15af6df89a
joyrudra-itobuz <joyrudra@itobuz.com>
2023-02-20 11:16:07 +0530

    Carousel Autoplay

--- js/main.js
+++ js/main.js
@@ -69,10 +69,18 @@ const subscribeDetailsValidate = () => {
 // SWIPPER CODE
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 100,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
